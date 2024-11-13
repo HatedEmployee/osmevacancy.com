8c6a5a5c720dfebd84a28401b6a815edb52954ad
HatedEmployee <136454713+HatedEmployee@users.noreply.github.com>
2024-11-13 12:23:48 +0545

    Updated vacancy (13th November, 2024)

--- data.js
+++ data.js
@@ -4,19 +4,21 @@ This file contains just the information about the demands, their salar and job n
 
 const arrEnglishBoys = [
 	["\t\tDesignation / Job\t\t", "\t\tSalary\t\t"],
-	["Head Chef", "Euro 1200 - Euro 1800"],
-	["Italian Cook", "Euro 800 - Euro 1000"],
-	["Continental Cook", "Euro 800 - Euro 1000"],
-	["Sushi Cook", "Euro 800 - Euro 1000"],
-	["Pastry Cook", "Euro 800 - Euro 1000"],
+	// ["Head Chef", "Euro 1200 - Euro 1800"],
+	// ["Italian Cook", "Euro 800 - Euro 1000"],
+	// ["Continental Cook", "Euro 800 - Euro 1000"],
+	// ["Sushi Cook", "Euro 800 - Euro 1000"],
+	// ["Pastry Cook", "Euro 800 - Euro 1000"],
 	["Cook", "Euro 800 - Euro 1000"],
 	["Kitchen Helper / Assistant Cook", "Euro 700 - Euro 800"],
-	["Baker", "Euro 750 - Euro 1000"],
+	// ["Baker", "Euro 750 - Euro 1000"],
+	["Barista", "Euro 700 - Euro 800"],
 	["Bartender", "Euro 750 - Euro 1000"],
-	["Pizza Maker", "Euro 750 - Euro 800"],
+	// ["Pizza Maker", "Euro 750 - Euro 800"],
 	["Waiter", "Euro 700 - Euro 800"],
-	["Welder", "Euro 750 - Euro 1000"],
-	["Eletricial", "Euro 750 - Euro 900"],
+	// ["Welder", "Euro 750 - Euro 1000"],
+	// ["Eletricial", "Euro 750 - Euro 900"],
+	["Butcher", "Euro 920"],
 ]
 
 const arrEnglishGirls = [
@@ -24,8 +26,10 @@ const arrEnglishGirls = [
 	["Waitress", "Euro 700 - Euro 800"],
 	["Housekeeping / Chambermaid", "Euro 700 - Euro 800"],
 	["Receptionist", "Euro 800 - Euro 900"],
-	["Cashier", "Euro 800 - Euro 900"],
-	["Spa Therapist", "Euro 700 - Euro 1000"],
+	// ["Cashier", "Euro 800 - Euro 900"],
+	// ["Spa Therapist", "Euro 700 - Euro 1000"],
+	["Barista", "Euro 700 - Euro 800"],
+	["Bartender", "Euro 700 - Euro 800"],
 	["Housemaid", "Euro 400"],
 ]
 
@@ -42,7 +46,7 @@ const WebsiteDataEnglish = [
 	[
 		"Requirements",
 		"For Housemaids",
-		"1. This is a placeholder",
+		"1. Candidate may need to learn Greek Language.",
 		"Except Housemaid (For Cook, Waiter/Waitress, Chambermaid, etc.)",
 		"1. You must have a video of you working to your respective applied job. (For example, if you are applying as a waiter you must have a video showing you doing waiter job.\n\
 		2. You must have a self-introduction video (without any kind of questions).\n",
@@ -51,19 +55,21 @@ const WebsiteDataEnglish = [
 
 const arrNepaliBoys = [
 	["\t\tपदनाम / काम\t\t", "\t\tतलब\t\t"],
-	["हेद सेफ", "यूरो १२०० - यूरो १८००"],
-	["इटालियन कुक", "यूरो ८०० - यूरो १००० "],
-	["कन्टिनेन्टल कुक ", "यूरो ८०० - यूरो १००० "],
-	["सुशी कुक", "यूरो ८०० - यूरो १०००"],
-	["पेस्त्री कुक", "यूरो ८०० - यूरो १००० "],
+	// ["हेद सेफ", "यूरो १२०० - यूरो १८००"],
+	// ["इटालियन कुक", "यूरो ८०० - यूरो १००० "],
+	// ["कन्टिनेन्टल कुक ", "यूरो ८०० - यूरो १००० "],
+	// ["सुशी कुक", "यूरो ८०० - यूरो १०००"],
+	// ["पेस्त्री कुक", "यूरो ८०० - यूरो १००० "],
 	["कुक", "यूरो ८०० - यूरो १०००"],
 	["कित्चेन्न हेल्पर / अस्सिस्तेन्त कुक", "यूरो ७०० - यूरो ८००"],
-	["बेकर", "यूरो ७५० - यूरो १०००"],
+	// ["बेकर", "यूरो ७५० - यूरो १०००"],
+	["बारिस्ता", "यूरो ७०० - यूरो ८००"],
 	["बारतेंदर", "यूरो ७५० - यूरो १००० "],
-	["पिज्जा मेकर ", "यूरो ७५० - यूरो १००० "],
+	// ["पिज्जा मेकर ", "यूरो ७५० - यूरो १००० "],
 	["वेटर ", "यूरो ७०० - यूरो ८०० "],
-	["वेल्दर", "यूरो ७५० - यूरो १००० "],
-	["एलेक्त्रिसियन", "यूरो ७५० - यूरो ९०० "],
+	// ["वेल्दर", "यूरो ७५० - यूरो १००० "],
+	// ["एलेक्त्रिसियन", "यूरो ७५० - यूरो ९०० "],
+	["कसै / मासु काट्ने", "यूरो ९२०"],
 ]
 
 const arrNepaliGirls = [
@@ -71,8 +77,8 @@ const arrNepaliGirls = [
 	["वैत्रेस", "यूरो ७०० - यूरो ८००"],
 	["होउसकीपिंग / चेम्बर्मैद", "यूरो ७०० - यूरो ८००"],
 	["रेसेप्सनीस्त", "यूरो ८०० - यूरो ९००"],
-	["क्यसिएर", "यूरो ८०० - यूरो ९००"],
-	["स्पा थेरापिस्ट", "यूरो ७०० - यूरो १०००"],
+	// ["क्यसिएर", "यूरो ८०० - यूरो ९००"],
+	// ["स्पा थेरापिस्ट", "यूरो ७०० - यूरो १०००"],
 	["होउसमैद", "यूरो ४००"],
 ]
 
@@ -89,7 +95,7 @@ const WebsiteDataNepali = [
 	[
 		"आवश्यकताहरु",
 		"घरको कामको लागि जान लग्नु भएकोहरुको लागि",
-		"१. यहाँ लेखन बाकी छ",
+		"१. तपाईहरुले ग्रीक भाषा पढ्न पर्ने हुन्छ | ",
 		"घरको काम बाहेक अरु कामको लागि (कुक, च्याम्बरमेड, वेटर / वैत्रेस, आदिइत्यादी.)",
 		"१. तपाईहरु साङ्ग आफु जुन कामको लागि जान आट्नु भएको छ, त्यो कामको भिडियो हुनु पर्छ (उदाहरणको लागि, तपाई वेटर हो भने तपाईले कसैलाई सेवा गरेको भिडियो हुन पर्छ) | \n\
 		२. तपाई संग आफ्नो परीचय गरेको भिडियो हुन पर्छ | (भिडियोमा कुनै किसिमको प्रशन हुनुहुदैन)\n",
