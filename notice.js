5e7936ff41794a3e65f508bf11fded4a2ddf80cd
HatedEmployee <136454713+HatedEmployee@users.noreply.github.com>
2024-11-17 17:23:16 +0545

    updated Notice

--- notice.js
+++ notice.js
@@ -3,10 +3,10 @@ this just holds the data for notice nothing more
 */
 const NoticeEnglish = [
 	"Notice",
-	"1. There will be Interview on Sunday (17th Nov, 2024) for Barista (male & female), Waiter / Waitress, Bartender (male & female), Chambermaid (male & female). Interested Candidates can visit our office at 9:30 AM of Cyprus Time. \n",
+	"1. There will be Interview on Monday (18th Nov, 2024) for Bartender (Male / Female), Waitress (Female Only), Chambermaid (male & female). Interested Candidates can visit our office at 9:30 AM of Cyprus Time. \n",
 ]
 
 const NoticeNepali = [
 	"सुचना",
-	"१. अाइतबार (१७ नाेभेम्बर, २०२४) तारिकमा बारिस्ता (केटा, केटी), वेटर, वेत्रेस, बारतेन्दर (केटा, केटी), हाउसकिपीँग (केटा, केटी) कामका लागि हाम्राे अफिसमा अन्तरवार्ता हुदै छ, इछुक उम्मेदवारहरूले हाम्राे अफिसमा ९:३० बिहानी (साइप्रसकाे समय) अाउन सक्नु हुन्छ । ",
+	"१. सोमबार (१८ नोबेम्बर, २०२४) तारिकमा वेत्रेस(केटि मात्रै), बारतेन्दर (केटा, केटी), हाउसकिपीँग (केटा, केटी) कामका लागि हाम्रो अफिसमा अन्तरवार्ता हुदै छ, इछुक उम्मेदवारहरूले हाम्रो अफिसमा ९:३० बिहानी (साइप्रसको समय) आउन सक्नु हुन्छ । \n",
 ]
