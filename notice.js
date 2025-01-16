2cfe666ce7daa8cb8793eb2c4c59d5ee0ab8dade
HatedEmployee <136454713+HatedEmployee@users.noreply.github.com>
2025-01-16 13:27:53 +0545

    Update notice.js

--- notice.js
+++ notice.js
@@ -6,8 +6,8 @@ const NoticeEnglish = [
 	// "1. There will be Interview on Sunday (17th Nov, 2024) for Barista (male & female), Waiter / Waitress, Bartender (male & female), Chambermaid (male & female). Interested Candidates can visit our office at 9:30 AM of Cyprus Time. \n",
 	// "1. Due to upcoming Christmas Festival & New Year 2025, most of our agencies will be on holiday, thus, regular interviews for all sorts of work will be continued after 4th - 5th January, 2024.\n\
 	// Merry Christmas & A Happy New Year 2025.\n"
-	"1. There will be interview for Service Sectors (Cook, Waiter, Chambermaid / Housekeeping etc.) in a regular basis, except for Saturday, Sunday and Tuesday. The interview time is 9:00 - 9:30 AM (Cyprus Time). \n",
-	"\n\n * To query about interview, please contact at (977) 9849040279 during office hours (11:30 Morning - 3:50 Afternoon).",
+	"1. There will be interview for Service Sectors (Cook, Waiter, Chambermaid / Housekeeping etc.) in a regular basis, except for Saturday, Sunday and Tuesday. The interview time is 9:00 - 9:30 AM (Cyprus Time). \n\
+	\n\n * To query about interview, please contact at (977) 9849040279 during office hours (11:30 Morning - 3:50 Afternoon).",
 ]
 
 const NoticeNepali = [
@@ -15,6 +15,6 @@ const NoticeNepali = [
 	// "१. अाइतबार (१७ नाेभेम्बर, २०२४) तारिकमा बारिस्ता (केटा, केटी), वेटर, वेत्रेस, बारतेन्दर (केटा, केटी), हाउसकिपीँग (केटा, केटी) कामका लागि हाम्राे अफिसमा अन्तरवार्ता हुदै छ, इछुक उम्मेदवारहरूले हाम्राे अफिसमा ९:३० बिहानी (साइप्रसकाे समय) अाउन सक्नु हुन्छ । ",
 	// "1. आगामी क्रिसमस चाड र नयाँ वर्ष 2025 को कारणले गर्दा, हाम्रा धेरैजसो एजेन्सीहरू बिदामा हुनेछन्, त्यसैले, 4 - 5 जनवरी, 2024 पछि सबै प्रकारका कामहरूको लागि नियमित अन्तर्वार्ता जारी रहनेछ।\n\
 	// मेरी क्रिसमस र नयाँ वर्ष २०२५ को शुभकामना।\n"
-	"1. सेवा क्षेत्र (कुक, वेटर, चेम्बरमेड / हाउसकिपिङ आदि) को लागि शनिबार, आइतवार र मंगलबार बाहेक नियमित रूपमा अन्तर्वार्ता हुनेछ। अन्तर्वार्ता समय 9:00 - 9:30 AM (साइप्रस समय) हो।",
-	"\n\n * अन्तर्वार्ताको बारेमा सोधपुछ गर्न, कृपया कार्यालय समय (बिहान 11:30 - 3:50 दिउँसो) मा (977) 9849040279 मा सम्पर्क गर्नुहोस्।",
+	"1. सेवा क्षेत्र (कुक, वेटर, चेम्बरमेड / हाउसकिपिङ आदि) को लागि शनिबार, आइतवार र मंगलबार बाहेक नियमित रूपमा अन्तर्वार्ता हुनेछ। अन्तर्वार्ता समय 9:00 - 9:30 AM (साइप्रस समय) हो।\
+	\n\n * अन्तर्वार्ताको बारेमा सोधपुछ गर्न, कृपया कार्यालय समय (बिहान 11:30 - 3:50 दिउँसो) मा (977) 9849040279 मा सम्पर्क गर्नुहोस्।",
 ]
